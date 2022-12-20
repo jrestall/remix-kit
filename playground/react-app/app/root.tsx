e509d306d52774c35bc21649c8622f854cbfd517
jrestall <james.restall@gmail.com>
2022-12-21 00:40:52 +1300

    chore(docs): root loader example

--- playground/react-app/app/root.tsx
+++ playground/react-app/app/root.tsx
@@ -1,20 +1,26 @@
 import type { MetaFunction } from '@remix-run/node';
-import { Links, Meta, Outlet, Scripts } from '@remix-run/react';
+import { Links, Meta, Outlet, Scripts, useLoaderData } from '@remix-run/react';
 import { ReactRefresh } from '@remix-kit/react';
 import React from 'react';
-import stylesUrl from "./styles/tailwind.css?url"
+import stylesUrl from './styles/tailwind.css?url';
+import { json } from '@remix-run/node';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'My Remix App',
   viewport: 'width=device-width,initial-scale=1',
-});   
+});
+
+export async function loader() {
+  return json({ test: 'Root Loader Data Test' });
+}
 
 export function links() {
-  return [{ rel: "stylesheet", href: stylesUrl }]
+  return [{ rel: 'stylesheet', href: stylesUrl }];
 }
 
 export default function App() {
+  const data = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -25,6 +31,7 @@ export default function App() {
       <body>
         <Outlet />
         <Scripts />
+        {data.test}
       </body>
     </html>
   );
