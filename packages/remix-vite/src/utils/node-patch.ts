3ef14f2a8a32e20c59018d3ccdb49df10a448c0d
jrestall <james.restall@gmail.com>
2022-12-29 18:02:08 +1300

    fix(vite): node-patch formatting

--- packages/remix-vite/src/utils/node-patch.ts
+++ packages/remix-vite/src/utils/node-patch.ts
@@ -5,17 +5,17 @@
 // Since that doesn't provide any value to our users we suppress the warning.
 
 export function removeExperimentalFetchWarnings() {
-    const originalEmit = process.emit;
-    // @ts-expect-error - TS complains about the return type of originalEmit.apply
-    process.emit = function (name, data: any, ...args) {
-      if (
-        name === `warning` &&
-        typeof data === `object` &&
-        data.name === `ExperimentalWarning` &&
-        data.message.includes(`The Fetch API is an experimental feature.`)
-      )
-        return false;
-  
-      return originalEmit.apply(process, arguments as unknown as Parameters<typeof process.emit>);
-    };
-  }
+  const originalEmit = process.emit;
+  // @ts-expect-error - TS complains about the return type of originalEmit.apply
+  process.emit = function (name, data: any, ...args) {
+    if (
+      name === `warning` &&
+      typeof data === `object` &&
+      data.name === `ExperimentalWarning` &&
+      data.message.includes(`The Fetch API is an experimental feature.`)
+    )
+      return false;
+
+    return originalEmit.apply(process, arguments as unknown as Parameters<typeof process.emit>);
+  };
+}
